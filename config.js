f28d115be30c4b3709394cb7b315fcf8fbb306d3
Sandesh Bhandari <36527732+sandesh0418@users.noreply.github.com>
2019-02-21 22:02:05 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,21 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'nodejs_login1'
+  
 });
 connection.connect(function(err){
 if(!err) {
     console.log("Database is connected");
+	connection.query("CREATE DATABASE IF NOT EXISTS `nodejs_login1`",function(err,results){
+		
+		console.log("Database created");
+	})
+	
+	
+	connection.query("CREATE TABLE IF NOT EXISTS `users`",function(err,results){
+		console.log("Table created");
+		
+	});
 } else {
     console.log("Error while connecting with database");
 }
